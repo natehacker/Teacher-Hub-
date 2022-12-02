6529ddc30c8962fb52f804ee2e6e33814aa7d688
Devon Jones <linesbydevon@gmail.com>
2022-12-02 14:23:30 -0500

    updated header/body styles

--- portal/src/components/Header.jsx
+++ portal/src/components/Header.jsx
@@ -1,13 +1,20 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function Header () {
 
 
     return (
-        <div className="header">
+        <header>
             <img className="header-logo" src="https://ga-cms-production-herokuapp-com.global.ssl.fastly.net/assets/ga-lockup-98c1ddeda4a74d005f14cdf28b99c214dfda1b47c2c5f1141a48bee2ec21f3b0.png" alt =""/>
             
-            <p className="login-register"><Link to="/">Log In</Link> | <Link to ="/register">Register</Link></p>
-        </div>
+            <p className="login-register">
+                <NavLink to="/">
+                    <div className="navFlex"><span>Log In</span></div>
+                </NavLink>
+                <NavLink to ="/register">
+                    <div className="navFlex"><span>Register</span></div>
+                    </NavLink>
+            </p>
+        </header>
     )
 }
