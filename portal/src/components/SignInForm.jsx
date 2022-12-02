8aa9eced3bc98c8f73afa88b44e4d1dd3012dd44
tyler <tylerwade124@gmail.com>
2022-12-02 09:44:56 -0500

    signinform created

--- portal/src/components/SignInForm.jsx
+++ portal/src/components/SignInForm.jsx
@@ -1,5 +1,18 @@
 
 
 export default function SignInForm () {
-    
+    <div className="signin-form">
+        
+        <div className="form-header">
+            <h3>Welcome</h3>
+        </div>
+
+
+
+        <button className="login-button">LOG IN</button>
+
+        <div className="form-footer">
+            <small>Don't have an account? Register</small>
+        </div>
+    </div>
 }
