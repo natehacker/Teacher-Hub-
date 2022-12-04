91a0be0100043d1ba4a094a91a16e6e009f56fd1
Nathan louissaint <louissaintmark@gmail.com>
2022-12-04 15:39:49 -0500

    nate's branch

--- portal/src/components/Footer.jsx
+++ portal/src/components/Footer.jsx
@@ -5,8 +5,9 @@ export default function Footer () {
 
     return (
         <footer>
-            <h1>n</h1>
-            <img className="footer-logo" src="https://seeklogo.com/images/G/general-assembly-logo-D5C634F07A-seeklogo.com.png" alt=""/>
+           <a href="https://www.linkedin.com/feed/"><img className="footer-logo" src="https://cdn3.iconfinder.com/data/icons/sociocons/256/linkedin-sociocon.png" alt=""/></a>&nb
+           <a href ="https://generalassemb.ly/?&topic=&mkt_account_id=1056949875&mkt_campaign_id=691778069&mkt_ad_group_id=31181421810&mkt_device_type=c&mkt_keyword=general%20assembly&mkt_matchtype=e&mkt_placement=&mkt_ad_id=155524967456&mkt_network=g&mkt_target_id=aud-1433782429321:kwd-459322816&mkt_feed_item_id=&utm_source=google&utm_medium=paid-search-bra&utm_campaign=TS:TX:BRA:ATL:BR:GeneralAssembly&utm_content=campus-lead-lander&utm_term=general%20assembly&gclid=CjwKCAiAhKycBhAQEiwAgf19enPTGvomKrcC7oLdYQ5fvSzPkPX9-nUTVtrWD_-yX5C1R_3wTslxdhoC7qEQAvD_BwE&gclsrc=aw.ds"><img className="footer-logo" src="https://seeklogo.com/images/G/general-assembly-logo-D5C634F07A-seeklogo.com.png" alt=""/></a>&nb
+            <a href="https://github.com/natehacker/Teacher-Hub-"><img className="footer-logo" src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/71-github-512.png" alt=""/></a>
         </footer>
     )
 }
