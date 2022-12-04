78e4eef2afb903490f88296c209f074bede48453
Devon Jones <linesbydevon@gmail.com>
2022-12-04 17:08:45 -0500

    login functionality added and working afaik

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,13 +1,37 @@
+import { useState, useEffect } from 'react';
+import {CheckSession} from './services/Auth';
 import './App.css';
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  const checkToken = async ()=> {
+    const user = await CheckSession();
+    setUser(user);
+    toggleAuthenticated(true);
+  }
+
+  useEffect(()=>{
+    const token = localStorage.getItem('token');
+    if (token){
+      checkToken()
+    }
+  }, [])
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main setUser={setUser} toggleAuthenticated={toggleAuthenticated}/>
       <Footer />
     </div>
   );
--- portal/src/components/Main.jsx
+++ portal/src/components/Main.jsx
@@ -4,13 +4,13 @@ import RegisterForm from "./RegisterForm"
 import Portal from "./Portal"
 
 
-export default function Main () {
+export default function Main ({setUser, toggleAuthenticated}) {
 
 
     return (
         <main className="main">
             <Routes>
-                <Route path="/" element = {<SignInForm />} />
+                <Route path="/" element = {<SignInForm setUser={setUser} toggleAuthenticated={toggleAuthenticated}/>} />
                 <Route path="/register" element = {<RegisterForm />}/>
                 <Route path="/portal" element = {<Portal/>}/>
             </Routes>
