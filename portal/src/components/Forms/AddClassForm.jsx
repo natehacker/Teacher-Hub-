d79562a78d913222cda0187b7e3b45964b015b48
tyler <tylerwade124@gmail.com>
2022-12-05 15:13:44 -0500

    useState on forms and password validation/doesn't allow null

--- portal/src/components/Forms/AddClassForm.jsx
+++ portal/src/components/Forms/AddClassForm.jsx
@@ -1,14 +1,29 @@
 import FormHeader from "./FormHeader";
 import FormWindow from "./FormWindow";
 import GAButton from "./GAButton";
+import { useState } from "react"
 
 export default function AddClassForm(props){
+  const [formValues, setFormValues] = useState({
+    name: ""
+  })
+
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues, [e.target.name]:e.target.value
+    })
+  }
+
+  const handleSubmit = () => {
+
+  }
+  
   return(
   <FormWindow>
     <FormHeader>Add Class</FormHeader>
         <form>
             <label htmlFor="name">Name : </label>
-            <input type="text" name="name"></input>
+            <input type="text" name="name" onChange={handleChange}></input>
             <GAButton>ADD CLASS</GAButton>
         </form>
 
--- portal/src/components/Forms/AddStudentForm.jsx
+++ portal/src/components/Forms/AddStudentForm.jsx
@@ -1,16 +1,30 @@
 import FormHeader from "./FormHeader";
 import FormWindow from "./FormWindow";
 import GAButton from "./GAButton";
+import { useState } from "react"
 
 export default function AddStudentForm(props){
+  const [formValues, setFormValues] = useState({
+    firstName: "",
+    lastName: ""
+  })
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues, [e.target.name]:e.target.value
+      
+    })
+  }
+  const handleSubmit = () => {
+
+  }
   return(
   <FormWindow>
     <FormHeader>Add Student</FormHeader>
         <form>
             <label htmlFor="firstName">First Name : </label>
-            <input type="text" name="firstName"></input>
+            <input type="text" name="firstName" onChange={handleChange}></input>
             <label htmlFor="lastName">Last Name : </label>
-            <input type="text" name="lastName"></input>
+            <input type="text" name="lastName" onChange={handleChange}></input>
             <GAButton>ADD STUDENT</GAButton>
         </form>
 
