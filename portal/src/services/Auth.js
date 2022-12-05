6976bd422346baf025ced4a8ed72adb2d0ff387e
Devon Jones <linesbydevon@gmail.com>
2022-12-05 12:06:20 -0500

    created first modal (addClass)

--- portal/src/services/Auth.js
+++ portal/src/services/Auth.js
@@ -5,6 +5,7 @@ export const SignInUser = async (data) => {
     const res = await Client.post('api/teachers/login', data)
     // Set the current signed in users token to localStorage
     localStorage.setItem('token', res.data.token)
+    console.log(res.data)
     return res.data.user
   } catch (error) {
     throw error
