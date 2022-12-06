49debe704ea546441b2f396f2b14a958a0e06bd8
Devon Jones <linesbydevon@gmail.com>
2022-12-06 18:26:32 -0500

    created assignment component and updated some preliminary styles

--- portal/src/components/elements/Assignment.jsx
+++ portal/src/components/elements/Assignment.jsx
@@ -2,17 +2,30 @@ import SVGUpdate from "./SVGs/SVGUpdate";
 import SVGDelete from "./SVGs/SVGDelete";
 
 export default function Assignment(){
+  const showHide = e=>{
+    e.currentTarget.parentElement.classList.toggle("expanded")
+  }
   return(
     <section className="assignment">
-      <div>
-      <div className="studentInfo">
-        <h4>Assignment</h4>
+      <div className="assignmentHeader" onClick={showHide}>
+      <div className="assignmentInfo">
+        <p>Assignment</p>
         </div>
-        <div className="studentCRUD">
+        <div className="assignmentLinks">
+          <a href="#">Github</a>&nbsp;|&nbsp;<a href="#">Deployed</a>
+          <SVGUpdate />
+        </div>
+        <div className="assignmentCRUD">
           <SVGUpdate />
           <SVGDelete />
         </div>
-      </div>
+        </div>
+        {/* <div className="assignment_more">
+     
+    
+          testing
+        </div> */}
+      
     </section>
   )
 }
