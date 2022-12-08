5ca85baf3e035dc427369805ee4d83604ae9b5cc
Devon Jones <linesbydevon@gmail.com>
2022-12-07 20:41:49 -0500

    added welcome user.firstname to header if logged in

--- portal/src/components/Header.jsx
+++ portal/src/components/Header.jsx
@@ -7,14 +7,22 @@ export default function Header ({user, authenticated, handleLogOut}) {
         <header>
             <img className="header-logo" src="https://ga-cms-production-herokuapp-com.global.ssl.fastly.net/assets/ga-lockup-98c1ddeda4a74d005f14cdf28b99c214dfda1b47c2c5f1141a48bee2ec21f3b0.png" alt =""/>
             
-           
+
+           <div className="headerInfo">
+                {
+                    (user && authenticated) ? (
+                        <div className="userDiv">
+                            <p>Welcome<br></br>{user.firstName}</p>
+                        </div>
+                    ): false
+                }
             <nav>
               {
                (user && authenticated) ? (
                 
                 <NavLink onClick={handleLogOut} to="/">
                     <div className="navFlex"><span>Log Out</span></div>
-                    </NavLink> 
+                </NavLink> 
                
                 ) : (location.pathname==="/about" || location.pathname==="/registered")?(
                     <NavLink to="/">
@@ -32,8 +40,8 @@ export default function Header ({user, authenticated, handleLogOut}) {
                 )
 
               }  
-            
             </nav>
+        </div>
         </header>
     )
 }
