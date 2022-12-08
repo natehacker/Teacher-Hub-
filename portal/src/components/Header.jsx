b424d949ca6a1d58634350f04241f2133312f4c8
Devon Jones <linesbydevon@gmail.com>
2022-12-08 11:32:40 -0500

    added svg for 'digital classroom' logo lockup

--- portal/src/components/Header.jsx
+++ portal/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import { NavLink, useLocation } from 'react-router-dom'
+import SVGDigitalClassroom from './elements/SVGs/SVGDigitalClassroom';
 
 export default function Header ({user, authenticated, handleLogOut}) {
     const location = useLocation();
     return (
         <header>
-            <img className="header-logo" src="https://ga-cms-production-herokuapp-com.global.ssl.fastly.net/assets/ga-lockup-98c1ddeda4a74d005f14cdf28b99c214dfda1b47c2c5f1141a48bee2ec21f3b0.png" alt =""/>
-            
+       
+
+            <SVGDigitalClassroom/>
 
            <div className="headerInfo">
                 {
